a676f2305c33f1ea8ae274a6f4bec8d5a0fa408d
Sanskar <iamsanskar92@gmail.com>
2026-04-19 11:04:13 +0530

    feat: Add packet callback and logging interface

--- network_analyzer.py
+++ network_analyzer.py
@@ -1,9 +1,20 @@
 from scapy.all import *
+import logging
+
+logging.basicConfig(filename=''network_traffic.log'', level=logging.INFO, format=''%(asctime)s - %(message)s'')
+packet_count = 0
+
+def packet_callback(packet):
+    global packet_count
+    max_packets = 20
+    if packet_count < max_packets:
+        logging.info(packet.summary())
+        packet_count += 1
 
 def main():
     print("Starting packet capture. Press Ctrl+C to stop.")
     try:
-        sniff(count=10)
+        sniff(prn=packet_callback, store=0)
     except KeyboardInterrupt:
         print("Packet capture stopped.")
 
